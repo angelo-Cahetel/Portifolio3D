4d64fcd211193f3063ed3006fa2f5b550eba1951
angelo-Cahetel <angelo030825@gmail.com>
2023-03-14 18:01:40 -0300

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,70 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import cloud from "./company/cloud.png";
+import calculadora from "./company/calculadora.png";
+import receita from "./company/receita.png";
+import logoco from "./company/logoco.png";
+
+import carrent from "./weaterApp.png";
+import jobit from "./ParticlesJS.png";
+import tripguide from "./appeperia.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  cloud,
+  calculadora,
+  receita,
+  logoco,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,257 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    cloud,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    calculadora,
+    receita,
+    logoco,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    // {
+    //   name: "Redux Toolkit",
+    //   icon: redux,
+    // },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    // {
+    //   name: "MongoDB",
+    //   icon: mongodb,
+    // },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    // {
+    //   name: "figma",
+    //   icon: figma,
+    // },
+    // {
+    //   name: "docker",
+    //   icon: docker,
+    // },
+  ];
+  
+  const experiences = [
+    {
+      title: "Weater app",
+      company_name: "Personal project",
+      icon: cloud,
+      iconBg: "#383E56",
+      date: "Feb 2023",
+      points: [
+        "I developed a weather app using java scrip.",
+        "A fully functional application that allows you to check the temperature of any country.",
+        "Using functional design for the best user experience",
+      ],
+    },
+    {
+      title: "calculator",
+      company_name: "Personal Project",
+      icon: calculadora,
+      iconBg: "#383E56",
+      date: "Jan 2023",
+      points: [
+        "I used JavaScript to develop a calculator.",
+        "I used JavaScript to develop a fully functional calculator.",
+        "I employed a modern and responsive design to enhance the user experience.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "Personal Project",
+      icon: receita,
+      iconBg: "#383E56",
+      date: "Sept 2022",
+      points: [
+        "I used HTML and CSS to develop a recipe website.",
+        "Utilizing assets to simplify website maintenance.",
+        "The website is completely responsive, catering to both web and mobile users.",
+      ],
+    },
+    {
+      title: "ThreeJS 3D portifolio",
+      company_name: "Personal Project",
+      icon: logoco,
+      iconBg: "#383E56",
+      date: "Present",
+      points: [
+        "I used React.js, Three.js, and other technologies to develop and maintain this website.",
+        "I developed this personal website to serve as a portfolio for my resume.",
+        "Using ThreeJS to create and animate 3d objects.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Weater App",
+      description:
+        "The weather application was developed to help users prepare for weather conditions before leaving their homes.",
+      tags: [
+        {
+          name: "html",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "javaScript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/angelo-Cahetel/weather-App",
+    },
+    {
+      name: "Particles Morph",
+      description:
+        "In this application, I used JavaScript particles to create shapes and geometric figures.",
+      tags: [
+        {
+          name: "html",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "javaScript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/angelo-Cahetel/Particles-Morph",
+    },
+    {
+      name: "Apeperia",
+      description:
+        "For this project, I developed a website for selling technology courses.",
+      tags: [
+        {
+          name: "html",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "css",
+          color: "green-text-gradient",
+        }
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/angelo-Cahetel/projeto-apeperia",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
